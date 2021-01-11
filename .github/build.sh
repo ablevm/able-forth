7c397bf94b59ccb97d562f507109ab8040b11a58
Mark Smith <mark.smith@ablevm.org>
2021-01-11 17:59:39 +0100

    Unroll subshells

--- .github/build.sh
+++ .github/build.sh
@@ -1,18 +1,39 @@
-mkdir build
-cd build
+#!/bin/sh
+
+set -e
 
 sudo apt-get update
 sudo apt-get install bmake clang
+
+mkdir build
+cd build
 
 git clone https://github.com/ablevm/libable
 git clone https://github.com/ablevm/able
 git clone https://github.com/ablevm/forth-img
 git clone https://github.com/ablevm/forth-scr
 
-(cd libable && cp config.mk.def config.mk && bmake -DCOMPAT_LINUX && sudo bmake install)
-(cd able && cp config.mk.def config.mk && bmake -DCOMPAT_LINUX && sudo bmake install)
-(cd forth-scr && bmake && sudo bmake install)
-(cd forth-img && bmake && sudo bmake install)
+cd libable
+cp config.mk.def config.mk
+bmake -DCOMPAT_LINUX
+sudo bmake install
+cd ..
+
+cd able
+cp config.mk.def config.mk
+bmake -DCOMPAT_LINUX
+sudo bmake install
+cd ..
+
+cd forth-img
+bmake
+sudo bmake install
+cd ..
+
+cd forth-scr
+bmake
+sudo bmake install
+cd ..
 
 cd ..
 rm -rf build
